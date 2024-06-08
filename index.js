63b2331c187024dbedadf778085b89afe3d764a1
shayykh <shayanshaikh2345@gmail.com>
2024-06-09 01:54:05 +0500

    Fixed grammar issues

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const answers = await inquirer.prompt([
     {
         type: "number",
         name: "guessNumber",
-        message: "Guess a number between 1 and 5: ",
+        message: "Guess a number from 1-5: ",
     }
 ]);
 let randomNumber = Math.floor(Math.random() * 5) + 1;
@@ -13,5 +13,5 @@ if (answers.guessNumber === randomNumber) {
     console.log("You guessed the right number!");
 }
 else {
-    console.log(`You guessed the wrong number! The correct number was ${randomNumber}`);
+    console.log(`You guessed the wrong number! The correct number was ${randomNumber}.`);
 }
--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const answers = await inquirer.prompt([
     {
         type: "number",
         name: "guessNumber",
-        message: "Guess a number between 1 and 5: ",
+        message: "Guess a number from 1-5: ",
     }
 ]);
 
@@ -16,5 +16,5 @@ let randomNumber = Math.floor(Math.random() * 5) + 1;
 if (answers.guessNumber === randomNumber) {
     console.log("You guessed the right number!");
 } else {
-    console.log(`You guessed the wrong number! The correct number was ${randomNumber}`);
+    console.log(`You guessed the wrong number! The correct number was ${randomNumber}.`);
 }
